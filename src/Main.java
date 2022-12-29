c797c8aee20d7ae56bd308a0534b51f645acc2c8
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-29 23:21:04 +0300

    сделано 2 задания из 3х

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //task1
+        System.out.println("задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName  = "Ivanov";
+        String fullName = "Ivanov " + "Ivan " + "Ivanovich";
+        System.out.println("ФИО сотрудника — " + fullName);
 
-        System.out.println("Hello world!");
+        //task2
+        System.out.println("задание 2");
+        String name = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + name);
+
+        //task3
+        System.out.println("задание 3");
+        
     }
 }
